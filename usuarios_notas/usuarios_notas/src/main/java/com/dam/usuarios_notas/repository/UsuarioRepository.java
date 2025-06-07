6cd1353b0ad1e3cca55d7d370af6f44f93b53907
colwhinamp <penaherediaandresmauricio@gmail.com>
2025-06-07 22:49:47 +0100

    v 0.5
    repositorio usuario

--- usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/repository/UsuarioRepository.java
+++ usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/repository/UsuarioRepository.java
@@ -1,5 +1,12 @@
 package com.dam.usuarios_notas.repository;
 
-public class UsuarioRepository {
-    
+import com.dam.usuarios_notas.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
