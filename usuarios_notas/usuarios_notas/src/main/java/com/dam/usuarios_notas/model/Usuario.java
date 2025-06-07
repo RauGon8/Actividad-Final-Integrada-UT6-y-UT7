b3d8d350f7973599881bb0a5de896225598e1c6c
colwhinamp <penaherediaandresmauricio@gmail.com>
2025-06-07 22:28:37 +0100

    v0.3

---
+++ usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/model/Usuario.java
@@ -0,0 +1,40 @@
+package com.dam.usuarios_notas.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String nombre;
+
+    @NotBlank(message = "El email no puede estar vacío")
+    @Email(message = "El email debe ser válido")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "El passwordHash no puede estar vacío")
+    private String passwordHash;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<Nota> notas = new ArrayList<>();
+
+    
+}
--- usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/repository/NotaRepository.java
+++ usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/repository/NotaRepository.java
@@ -1,3 +1,5 @@
+package com.dam.usuarios_notas.repository;
+
 public class NotaRepository {
     
 }
