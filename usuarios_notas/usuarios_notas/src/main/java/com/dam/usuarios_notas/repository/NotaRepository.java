9223d7f516e8daf49c9e1f14e773a1ac09b907dd
RauGon8 <raulgonzalezherrera8@gmail.com>
2025-06-07 22:54:14 +0100

    v 0.6

--- usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/repository/NotaRepository.java
+++ usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/repository/NotaRepository.java
@@ -1,5 +1,15 @@
 package com.dam.usuarios_notas.repository;
 
-public class NotaRepository {
-    
+import com.dam.usuarios_notas.model.Nota;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NotaRepository extends JpaRepository<Nota, Long> {
+    List<Nota> findByUsuarioId(Long usuarioId, Sort sort);
 }
+
