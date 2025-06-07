b13a0adceb576909177be17e32b90b316d05e175
colwhinamp <penaherediaandresmauricio@gmail.com>
2025-06-07 21:56:57 +0100

    commit incial

---
+++ usuarios_notas/usuarios_notas/src/main/java/com/dam/usuarios_notas/UsuariosNotasApplication.java
@@ -0,0 +1,13 @@
+package com.dam.usuarios_notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosNotasApplication.class, args);
+	}
+
+}
---
+++ usuarios_notas/usuarios_notas/src/test/java/com/dam/usuarios_notas/UsuariosNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.usuarios_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
